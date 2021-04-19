4f05625aa16d7ab7128639026a73454afde3f497
Edgar M <evmelendrez@gmail.com>
2021-04-19 16:59:08 -0700

    Prepping for Github

--- Lab_3.12/main.cpp
+++ Lab_3.12/main.cpp
@@ -13,7 +13,7 @@ template<typename T>
 void selection_sort(vector<T> &vals);
 
 template<typename T>
-T getElement(vector<T> vals, int index);
+T getElement(vector<T> v, int index) {return v.at(index);}
 
 vector<char> createVector(){
     int vecSize = rand() % 26;
@@ -30,8 +30,7 @@ vector<char> createVector(){
 int main(){
 
     //Part B
-    try 
-    {
+    
         srand(time(0));
         vector<char> vals = createVector();
         char curChar;
@@ -41,18 +40,60 @@ int main(){
         {
             cout << "Enter a number: " << endl;
             cin >> index;
-            if (index >= vals.size() || index < 0)
+            // if (index >= vals.size() || index < 0)
+            // {
+            //     throw out_of_range("out of range exception occured"); 
+            // }
+            try 
             {
-                throw out_of_range("out of range exception occured"); 
+                vals.at(index);
+                curChar = getElement(vals,index);
+                cout << "Element located at " << index << ": is " << curChar << endl;           
             }
-            curChar = getElement(vals,index);
-            cout << "Element located at " << index << ": is " << curChar << endl;           
+            catch (const out_of_range& excpt)
+            {
+                cout <</* excpt.what() << */"out of range exception occured" << endl;         
+            }
         }
-    }
-    catch (const out_of_range& excpt)
-    {
-        cout << excpt.what() << endl;         
-    }
+
+//testing for new template
+    //     string curString;
+    //     vector<string> strs;
+    //     strs.push_back("Abc");
+    //     strs.push_back("abc");
+    //     strs.push_back("qwert");
+    //     strs.push_back("asdf");
+    //     strs.push_back("zxcv");
+    //     strs.push_back("dfgh");
+    //     strs.push_back("utry");
+    //     strs.push_back("rth5");
+    //     strs.push_back("34fgds");
+
+    //    //It seems like its working properly.
+    //    for(int i = 0; i < strs.size();i++)
+    //    {
+    //        cout<<strs.at(i)<<endl;
+    //    }
+    //    selection_sort(strs);
+    //    cout<<endl;
+    //     cout<<"selection sort"<<endl;
+    //    for(int i = 0; i < strs.size();i++)
+    //    {
+    //        cout<<strs.at(i)<<endl;
+    //    }
+
+    //     numOfRuns=10;
+    //     while(--numOfRuns >= 0)
+    //     {
+    //         cout << "Enter a number: " << endl;
+    //         cin >> index;
+    //         //curString = strs.at(index);//What does this do? So we can look at a certain index, just like the othe 
+    //         curString = getElement(strs, index);//Why this not working!??????????No Idea it sould work THERE WE GO!!! I am an idot, when I copy and pasted
+    //         //forgot to change the variable from char to a string NOICE
+    //         cout << "Element located at " << index << ": is " << curString << endl;           
+    //     }
+//end of testing for string
+    
     return 0;
 }//end of main
 
@@ -81,8 +122,3 @@ void selection_sort(vector<T> &vals)
     }
 }//end of selection sort
 
-template<typename T>
-T getElement(vector<T> vals, int index)
-{
-    return vals.at(index);
-}
