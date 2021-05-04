8406173f91fe79de7189da77751840c7bd242308
Edgar M <evmelendrez@gmail.com>
2021-05-04 14:47:11 -0700

    graphviz is working!

--- Labs/Lab_6.60/arithmeticExpression.cpp
+++ Labs/Lab_6.60/arithmeticExpression.cpp
@@ -1,7 +1,59 @@
 #include "arithmeticExpression.h"
 #include "stack.h"
+#include <string>
+#include <sstream>
 #include <fstream>
 
+/* Initializes an empty tree and sets the infixExpression
+    to the value of parameter passed in. */
+arithmeticExpression::arithmeticExpression(const string &input) {
+    root = nullptr;
+    infixExpression = input;
+}
+
+/* Converts the infixExpression to its equivalent postfix string
+    and then generates the tree and assigns the root node to the 
+    root data field. The key for the first node is 'a', 'b' for the second node and so on. */
+void arithmeticExpression::buildTree() {
+
+}
+
+/* Calls the recursive infix function. */
+void arithmeticExpression::infix() {
+
+}
+
+/* Calls the recursive prefix function. */
+void arithmeticExpression::prefix() {
+
+}
+
+/* Calls the recursive postfix function. */
+void arithmeticExpression::postfix() {
+
+}
+
+/* Calls the recursive visualizeTree function and generates the .png file using system call. */
+void arithmeticExpression::visualizeTree(const string &outputFilename){
+    ofstream outFS(outputFilename.c_str());
+    if(!outFS.is_open()){
+        cout<<"Error opening "<< outputFilename<<endl;
+        return;
+    }
+    outFS<<"digraph G {"<<endl;
+    visualizeTree(outFS,root);
+    outFS<<"}";
+    outFS.close();
+    string jpgFilename = outputFilename.substr(0,outputFilename.size()-4)+".jpg";
+    string command = "dot -Tjpg " + outputFilename + " -o " + jpgFilename;
+    system(command.c_str());
+}
+
+
+
+
+/* Helper function that returns an integer according to
+       the priority of the given operator. */
 int arithmeticExpression::priority(char op){
     int priority = 0;
     if(op == '('){
@@ -56,19 +108,4 @@ string arithmeticExpression::infix_to_postfix(){
         s.pop();
     }
     return oss.str();
-}
-
-void arithmeticExpression::visualizeTree(const string &outputFilename){
-    ofstream outFS(outputFilename.c_str());
-    if(!outFS.is_open()){
-        cout<<"Error opening "<< outputFilename<<endl;
-        return;
-    }
-    outFS<<"digraph G {"<<endl;
-    visualizeTree(outFS,root);
-    outFS<<"}";
-    outFS.close();
-    string jpgFilename = outputFilename.substr(0,outputFilename.size()-4)+".jpg";
-    string command = "dot -Tjpg " + outputFilename + " -o " + jpgFilename;
-    system(command.c_str());
 }
